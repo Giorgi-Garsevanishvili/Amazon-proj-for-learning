18d98614150eb3a08f25734bb28a8566e9e44411
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-05-15 21:39:15 +0300

    end of testing introduction and cleanup testing codes

--- scripts/checkout/orderSummary.js
+++ scripts/checkout/orderSummary.js
@@ -28,7 +28,9 @@ export function renderOrderSummary()
 
 
     cartSummaryHTML += `
-      <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
+      <div class="cart-item-container 
+      js-cart-item-container
+      js-cart-item-container-${matchingProduct.id}">
       <div class="delivery-date">
         Delivery date: ${dateString}
       </div>
@@ -44,7 +46,7 @@ export function renderOrderSummary()
           <div class="product-price">
             $${formatCurency(matchingProduct.priceCents)}
           </div>
-          <div class="product-quantity">
+          <div class="product-quantity js-product-quantity-${matchingProduct.id}">
             <span>
               Quantity: <span class="quantity-label js-quantity-label-${matchingProduct.id}">${cartItem.quantity}</span>
             </span>
@@ -53,7 +55,9 @@ export function renderOrderSummary()
             </span>
             <input class="quantity-input js-quantity-input-${matchingProduct.id}">
               <span class="save-quantity-link link-primary js-save-link" data-product-id="${matchingProduct.id}">Save</span>
-            <span class="delete-quantity-link link-primary js-delete-link " data-product-id="${matchingProduct.id}">
+            <span class="
+            js-delete-link-${matchingProduct.id} 
+            delete-quantity-link link-primary js-delete-link " data-product-id="${matchingProduct.id}">
               Delete
             </span>
           </div>
