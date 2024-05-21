b3cf8fde1b93471129c3350d77d08f83dae1d448
Giorgi Garsevanishvili <george.garsevanidi@gmail.com>
2024-05-21 23:22:37 +0300

    OOP-Class-private-property

--- data/cart-class.js
+++ data/cart-class.js
@@ -2,16 +2,16 @@ import {validDeliveryOption} from './deliveryOptions.js';
 
 class Cart {
   cartItems;
-  localStorageKey;
+  #localStorageKey;
 
   constructor(localStorageKey) {
-    this.localStorageKey = localStorageKey;
-    this.loadFromStorage();
+    this.#localStorageKey = localStorageKey;
+    this.#loadFromStorage();
   }
 
-  loadFromStorage() {
+  #loadFromStorage() {
     this.cartItems = 
-  JSON.parse(localStorage.getItem(this.localStorageKey)) ;
+  JSON.parse(localStorage.getItem(this.#localStorageKey)) ;
   
   if (!this.cartItems) {
     this.cartItems = [{
